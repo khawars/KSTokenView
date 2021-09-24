bcf6f0feb0f092994debe79fe6012c94e724df18
Ajay Mann <ajaymann7@gmail.com>
2021-09-24 13:36:32 +0530

    Revert "Extend tableview to keyboard top. TODO : Keyboard height calculation"
    This reverts commit 0130b3f0620bc54da959d92e618e79dd860cc39d.

--- KSTokenView/KSTokenView.swift
+++ KSTokenView/KSTokenView.swift
@@ -95,14 +95,14 @@ open class KSTokenView: UIView {
    //MARK: - Private Properties
    //__________________________________________________________________________________
    //
-   @objc open var _tokenField: KSTokenField!
+   fileprivate var _tokenField: KSTokenField!
    fileprivate var _searchTableView: UITableView = UITableView(frame: .zero, style: UITableView.Style.plain)
    fileprivate var _resultArray = [AnyObject]()
    fileprivate var _showingSearchResult = false
-   fileprivate var _indicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorView.Style.gray)
+   fileprivate var _indicator = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.gray)
    fileprivate var _searchResultHeight: CGFloat = 200.0
    fileprivate var _lastSearchString: String = ""
-   fileprivate var _intrinsicContentHeight: CGFloat = UIViewNoIntrinsicMetric
+   fileprivate var _intrinsicContentHeight: CGFloat = UIView.noIntrinsicMetric
    
    //MARK: - Public Properties
    //__________________________________________________________________________________
@@ -426,7 +426,7 @@ open class KSTokenView: UIView {
    }
    
     override open var intrinsicContentSize : CGSize {
-      return CGSize(width: UIViewNoIntrinsicMetric, height: _intrinsicContentHeight)
+        return CGSize(width: UIView.noIntrinsicMetric, height: _intrinsicContentHeight)
     }
     
     //MARK: - Public Methods
